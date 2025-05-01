903ed5fee533a4aad19a97c071ef46ec86f7e18e
Jaakko Heusala <jhh@hg.fi>
2025-05-01 06:10:52 -0700

    refactor(io): split IO tool into separate read and write files

--- pkg/tools/io/write.go
+++ pkg/tools/io/write.go
@@ -9,22 +9,9 @@ import (
 	"gendo/pkg/log"
 )
 
-// ReadTool implements the tools.Tool interface for file reading
-type ReadTool struct {
-	basePath string
-}
-
 // WriteTool implements the tools.Tool interface for file writing
 type WriteTool struct {
 	basePath string
-}
-
-// NewReadTool creates a new file reading tool
-func NewReadTool(basePath string) *ReadTool {
-	log.Debug("Creating new read tool with base path: %q", basePath)
-	return &ReadTool{
-		basePath: basePath,
-	}
 }
 
 // NewWriteTool creates a new file writing tool
@@ -33,31 +20,6 @@ func NewWriteTool(basePath string) *WriteTool {
 	return &WriteTool{
 		basePath: basePath,
 	}
-}
-
-// Process implements the tools.Tool interface for ReadTool
-func (t *ReadTool) Process(input string) (string, error) {
-	log.Debug("Processing read input: %q", input)
-	
-	if input == "" {
-		log.Debug("Empty input provided")
-		return "", fmt.Errorf("no file path provided")
-	}
-
-	filePath := input
-	if t.basePath != "" {
-		filePath = filepath.Join(t.basePath, input)
-		log.Debug("Using full file path: %q", filePath)
-	}
-
-	content, err := os.ReadFile(filePath)
-	if err != nil {
-		log.Debug("Failed to read file %q: %v", filePath, err)
-		return "", fmt.Errorf("failed to read file: %v", err)
-	}
-
-	log.Debug("Successfully read %d bytes from %q", len(content), filePath)
-	return string(content), nil
 }
 
 // Process implements the tools.Tool interface for WriteTool
